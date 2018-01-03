a6207b27de79c43245c062f113b772991472278c
Perla Showell <perlashowell@gmail.com>
2018-01-02 23:05:14 -0500

    finished

--- server.js
+++ server.js
@@ -4,14 +4,12 @@ var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var logger = require('morgan'); // for debugging
 var request = require('request'); // for web-scraping
 var cheerio = require('cheerio'); // for web-scraping
 
 
 // Initialize Express for debugging & body parsing
 var app = express();
-app.use(logger('dev'));
 app.use(bodyParser.urlencoded({
   extended: false
 }))
