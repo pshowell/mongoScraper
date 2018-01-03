36846253f99eaf753946736978ebcb5289c3a673
Perla Showell <perlashowell@gmail.com>
2018-01-03 12:09:14 -0500

    db

--- server.js
+++ server.js
@@ -26,12 +26,11 @@ app.set('view engine', 'handlebars');
 // ---------------------------------------------------------------------------------------------------------------
 // Connect to localhost if not a production environment
 if(process.env.NODE_ENV == 'production'){
-  mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
+  mongoose.connect("mongodb://<perlashowell>:@ds239047.mlab.com:39047/heroku_cdqfkfxm");
 }
 else{
-  mongoose.connect('mongodb://localhost/news-scraper');
+  mongoose.connect("mongodb://<perlashowell>:@ds239047.mlab.com:39047/heroku_cdqfkfxm");
   // YOU CAN IGNORE THE CONNECTION URL BELOW (LINE 41) THAT WAS JUST FOR DELETING STUFF ON A RE-DEPLOYMENT
-  //mongoose.connect('mongodb://heroku_60zpcwg0:ubn0n27pi2856flqoedo9glvh8@ds119578.mlab.com:19578/heroku_60zpcwg0');
 }
 var db = mongoose.connection;
 
